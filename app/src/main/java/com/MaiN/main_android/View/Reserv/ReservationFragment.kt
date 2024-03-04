56d92d1e7586118260915acd33e3d7ce7eefc9ef
Gael-Android <gael.urssu@gmail.com>
2024-03-04 23:11:24 +0900

    add : Compose용 fragment 생성

---
+++ app/src/main/java/com/MaiN/main_android/View/Reserv/ReservationFragment.kt
@@ -0,0 +1,35 @@
+package com.MaiN.main_android.View.Reserv
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import com.MaiN.main_android.R
+
+class ReservationFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.reservation_compose_fragment, container, false)
+        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+        composeView.apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                // In Compose world
+                MaterialTheme {
+                    ReservationScreen()
+                }
+            }
+        }
+        return view
+    }
+}

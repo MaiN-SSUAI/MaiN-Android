8a4a8287d4494e516ee2625018929448492e18de
Gael-Android <gael.urssu@gmail.com>
2024-03-04 23:07:42 +0900

    add : 네트워크 요청 관련 객체생성

--- app/src/main/java/com/MaiN/main_android/retrofit/ReservDataclass.kt
+++ app/src/main/java/com/MaiN/main_android/retrofit/ReservDataclass.kt
@@ -1,20 +1,75 @@
 package com.MaiN.main_android.retrofit
 
+import com.MaiN.main_android.View.Reserv.state.CellUiState
 import com.google.gson.annotations.SerializedName
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.util.Locale
 
-class ReservDataclass : ArrayList<ReservDataclass.ReservDataclassItem>(){
+class ReservDataclass : ArrayList<ReservDataclass.ReservDataclassItem>() {
     data class ReservDataclassItem(
         @SerializedName("end")
         val end: String,
-        @SerializedName("end_pixel")
-        val end_pixel: String,
+//        @SerializedName("end_pixel")
+//        val end_pixel: String,
         @SerializedName("eventid")
         val eventid: String,
         @SerializedName("start")
         val start: String,
-        @SerializedName("start_pixel")
-        val start_pixel: String,
+//        @SerializedName("start_pixel")
+//        val start_pixel: String,
         @SerializedName("summary")
         val summary: String
     )
+}
+
+fun ReservDataclass.toCellUiStateList() = map {
+    CellUiState(
+        startHour = convertIsoDateTimeToHour(it.start),
+        startMin = convertIsoDateTimeToMinute(it.start),
+        endHour = convertIsoDateTimeToHour(it.end),
+        endMin = convertIsoDateTimeToMinute(it.end),
+        summary = it.summary,
+        koreaStyleTime = convertIsoDateTimeToKoreanDate(it.start),
+        eventId = it.eventid
+    )
+}
+
+fun convertIsoDateTimeToHour(isoDateTime: String): Int {
+    val customFormatter = DateTimeFormatterBuilder()
+        .appendPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+        .appendOffset("+HH:MM", "+00:00") // Handles different offset formats
+        .toFormatter()
+
+    val localDateTime =
+        LocalDateTime.parse(isoDateTime, customFormatter)
+    return localDateTime.hour
+}
+
+fun convertIsoDateTimeToMinute(isoDateTime: String): Int {
+    val customFormatter = DateTimeFormatterBuilder()
+        .appendPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+        .appendOffset("+HH:MM", "+00:00") // Handles different offset formats
+        .toFormatter()
+
+    val localDateTime =
+        LocalDateTime.parse(isoDateTime, customFormatter)
+    return localDateTime.minute
+}
+
+fun convertIsoDateTimeToKoreanDate(isoDateTime: String): String {
+    // Parse the ISO_DATE_TIME string to a ZonedDateTime object
+    val zonedDateTime = ZonedDateTime.parse(isoDateTime)
+
+    // Convert time zone to Korea Standard Time (KST)
+    val koreanTimeZone = zonedDateTime.withZoneSameInstant(ZoneId.of("Asia/Seoul"))
+
+    // Define a custom pattern for the Korean date format
+    val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 EEEE", Locale.KOREAN)
+
+    // Format and return the date in Korean format
+    return koreanTimeZone.format(formatter)
 }
--- app/src/main/java/com/MaiN/main_android/retrofit/RetrofitConnection.kt
+++ app/src/main/java/com/MaiN/main_android/retrofit/RetrofitConnection.kt
@@ -1,21 +1,38 @@
 package com.MaiN.main_android.retrofit
 
+import com.google.gson.GsonBuilder
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 //레트로핏 객체를 생성하는 클래스
 class RetrofitConnection {
+
+
     companion object {
-//        private const val BASE_URL = "http://10.0.2.2:8080/"
+        //        private const val BASE_URL = "http://10.0.2.2:8080/"
         private const val BASE_URL = "http://43.203.195.189/"
         private var INSTANCE: Retrofit? = null
 
-        fun getInstance() : Retrofit {
-            if(INSTANCE == null) {
+        private val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = okhttp3.OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+        }.build()
+
+
+        fun getInstance(): Retrofit {
+            if (INSTANCE == null) {
                 INSTANCE = Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .addConverterFactory(
+                        GsonConverterFactory.create(
+                            GsonBuilder().setLenient().create()
+                        )
+                    )
                     .build()
             }
             return INSTANCE!!
